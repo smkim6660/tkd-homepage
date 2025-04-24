d89e6fb3c319dd53c56387ae762262c290776ee0
smkim6660 <sjo414800@gmail.com>
2025-04-24 14:58:38 +0900

    Initial commit: Taekwondo 프로젝트 초기 셋업

---
+++ src/main/java/com/tkd/homepage/tkd_homepage/TkdHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.tkd.homepage.tkd_homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkdHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkdHomepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkd/homepage/tkd_homepage/TkdHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkd.homepage.tkd_homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkdHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
